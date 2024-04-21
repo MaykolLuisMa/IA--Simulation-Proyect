143268f792d6eba2e197d3a428e53f01df63b35b
Machi-san <maxibengochea67@gmail.com>
2024-04-21 19:52:46 -0400

    ad coments

--- nlp/nlp.py
+++ nlp/nlp.py
@@ -4,7 +4,7 @@ from nlp.prompt import *
 import json
 
 #respuesta del modelo a la query ingresada por el usuario
-def response(query: str) -> str:
+def response(sim: str, query: str) -> str:
   #cargar el server local
   client = OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio")
 
@@ -12,11 +12,11 @@ def response(query: str) -> str:
     model="NousResearch/Hermes-2-Pro-Mistral-7B-GGUF",
     messages=[
       {"role": "system", "content": SYSTEM_PROMPT},
-      {"role": "user", "content": build_prompt(query)}
+      {"role": "user", "content": build_prompt(sim, query)}
     ],
     temperature=0.7,
   )
   
-  json_result = completion.choices[0].message.content
-  result = json.loads(json_result)
+  json_result = completion.choices[0].message.content #respuesta en formato json
+  result = json.loads(json_result) #parsear de json a dict
   return result['result']
