6ddf06633eef3ad0412e3938082b1f7dd458a24b
Machi-san <maxibengochea67@gmail.com>
2024-04-21 19:23:04 -0400

    change prompt

--- nlp/prompt.py
+++ nlp/prompt.py
@@ -3,7 +3,8 @@ SYSTEM_PROMPT = "Eres un asistente que recibe un texto y responde a peticiones r
 
 #prompt generado con la query del usuario
 def build_prompt(query: str):
-  prompt = 'Necesito que me envies un objeto de tipo json solamente con los cambos "name" y "age"\n'
-  prompt += 'En dependencia de lo que ingrese el usuario procesa los datos y enviame el archivo json. Esta es la informacion del usuario:\n'
+  prompt = 'Necesito que me envies un objeto de tipo json solamente con el campo "result"\n'
+  prompt += 'En dependencia de lo que ingrese el usuario procesa los datos y almacena un resumen del procesamiento de la peticion.\n'
+  prompt += 'Esta es la peticion del usuario:\n'
   prompt += f'"{query}"'
   return prompt
